64777baa5221e900d9bb5e0f00735d930760cae9
saraygonm <zaryz2005@gmail.com>
2024-03-07 00:17:31 -0500

    controller

--- SpringBootHelloWorld/SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
+++ SpringBootHelloWorld/SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -3,10 +3,9 @@ package net.codejava.SpringBootHelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 @SpringBootApplication
@@ -16,16 +15,16 @@ public class SpringBootHelloWorldApplication {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
 
+	@CrossOrigin(origins = "http://mi-frontend.com", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 	@RequestMapping("/")
 	public String  home(){
 		return "<html><body><h1>Prueba</h1></body></html>";
 	}
 
+	@CrossOrigin(origins = "http://mi-frontend.com", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 	@RequestMapping("/new")
 	public String  otrometodo(){
 		return ":)";
 	}
-
-
 
 }
